b293c61918a4548f676222f07cf7d3f72d002fdb
Prajwolbh7 <60246105+Prajwolbh7@users.noreply.github.com>
2020-09-28 20:59:13 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,75 @@
+//randiomizer favourite food
+
+
+let friends = [{
+  name: "Mike",
+  food: "Pizza"
+},{
+  name: "Rocky",
+  food: "Pasta"
+},{
+  name: "Danny",
+  food: "Sandwich"
+},{
+  name: "Romeo",
+  food: "Burger"
+},{
+  name: "Ephny",
+  food: "rice"
+}];
+
+let randomIndex;
+let animating = false;
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(200);
+  textSize(40);
+
+  text("click to randomize", 100,100);
+
+
 }
 
 function draw() {
-  background(220);
-  rect(200,200,100,100);
+
+  if (animating == true){
+
+    fill(random(10,100));
+   ellipse(random(width),random(height),random(150,220));
+   rect(random(width),random(height),random(50,100));
+   circle(random(width),random(height),random(50,60));
+  }
+
+}
+function randiomizer() {
+
+  animating = false;
+  if(friends[0]){
+    // this displays random name and slice it out of array
+  background(random(200, 255));
+  randomIndex = int(random(friends.length))
+  // calls the ramdom index from friends array
+  //console.log(friends[randomIndex].name);
+  // to print in the canvas write text and give coordinates
+ text(`${friends[randomIndex].name}' s favourite food is
+ ${friends[randomIndex].food}`, 100,100);
+  //text(friends[randomIndex].name +"'s favourite color is'"+ friends[randomIndex].color, 100,100);
+
+  friends.splice(randomIndex,1);
+  //console.log(friends);
+}else {
+  background(random(200, 255));
+  text("NO favourite food!!!", 50,50);
+}
+
+}
+
+function mousePressed(){
+
+  animating = true;
+  setTimeout(randiomizer,2000);
+
+
 }
